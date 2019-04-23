3c7537becf49f530b000a4b3e7ea8f27fec45631
Anaclet Biquet <anaclet.biquet@gmail.com>
2019-04-23 11:36:15 +0200

    ok

---
+++ Singleton.java
@@ -0,0 +1,37 @@
+public class Singleton {
+
+    private static Singleton instance;
+
+    // TODO 1 : fix singleton bad implementation
+    public static Singleton getInstance() {
+         if (instance == null) {
+        // if no instance exists, create one
+        instance = new Singleton();
+    }
+    // in any case, return the same instance
+    return instance;
+    }
+
+    private Singleton(){
+
+    }
+
+    private Bank bank;    
+
+    
+    public void openAccount(int value) {
+        bank = new Bank(value);
+    }
+
+    public void deposit(int value) {
+        bank.setTotal(bank.getTotal() + value);
+    }
+
+    public void withdraw(int value) {
+        bank.setTotal(bank.getTotal() - value);
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+}
---
+++ Transactions.java
@@ -0,0 +1,29 @@
+public class Transactions {
+
+    public static void main(String[] args){
+        transactions();
+        balance();       
+    }
+
+    public static void transactions() {
+
+        Singleton singleton = Singleton.getInstance();
+        singleton.openAccount(9001);       
+
+        singleton.withdraw(300);
+        singleton.deposit(12);
+        singleton.deposit(54);
+        singleton.withdraw(47);
+    }
+
+    public static void balance() {
+
+        // TODO 2 : call singleton and get total
+        Singleton singleton = Singleton.getInstance();
+        Bank bank = singleton.getBank();      
+        System.out.printf("Your balance is:%d%n", bank.getTotal());
+    }
+}
+
+
+//singleton.openAccount(1000);
